5e112e029db94c90abb23b3ee15f74941b634e36
GentleDev0713 <vladimirleovac7@gmail.com>
2023-05-13 10:16:28 +0100

    commit

---
+++ src/components/layouts/Menu.js
@@ -0,0 +1,323 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
+import axios from "axios";
+
+const Menu = () => {
+  const [statuslist, setStatuslist] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://real-estate-backend-9ph8.onrender.com/admin/get-categories")
+      .then((res) => {
+        setStatuslist(res.data.result);
+      });
+  }, []);
+  return (
+    <Fragment>
+      {/* Logo */}
+      <Link
+        className="navbar-brand"
+        to="/"
+        onClick={() => window.location.replace("/")}
+      >
+        {" "}
+        <img
+          className="p-2"
+          src={process.env.PUBLIC_URL + "/assets/img/logo.png"}
+          alt="logo"
+        />{" "}
+      </Link>
+      {/* Menu */}
+      <ul className="navbar-nav">
+        <li className="menu-item menu-item-has-children">
+          <Link to="/about">About Us</Link>
+        </li>
+        <li className="menu-item menu-item-has-children items-center">
+          <Link to="#">
+            <Dropdown>
+              <Dropdown.Toggle
+                id="dropdown-basic"
+                style={{
+                  background: "white",
+                  color: "black",
+                  border: "none",
+                  fontWeight: "500",
+                  padding: "0px 2px",
+                }}
+              >
+                Services
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  href="#/action-1"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Distress">
+                    Help With Distress Home
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-2"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Planing">
+                    Financial Planing
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-3"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Modification">
+                    Loan Modification
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-4"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Management">
+                    Property Management
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-5"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Planner">
+                    Investment Planner
+                  </Link>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Link>
+        </li>
+
+        <li className="menu-item menu-item-has-children">
+          {/* <Link to="/sell">Sell</Link> */}
+          <Link to="#">
+            <Dropdown>
+              <Dropdown.Toggle
+                id="dropdown-basic"
+                style={{
+                  background: "white",
+                  color: "black",
+                  border: "none",
+                  fontWeight: "500",
+                  padding: "0px 2px",
+                }}
+              >
+                Properties
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                {statuslist.map((res) => {
+                  return (
+                    <Dropdown.Item
+                      href="#/action-1"
+                      style={{
+                        padding: "6% 2%",
+                        margin: "0px 8px",
+                        width: "95%",
+                      }}
+                    >
+                      <Link
+                        style={{ padding: "0" }}
+                        to={"/Property/" + res.name}
+                      >
+                        {res.name}
+                      </Link>
+                    </Dropdown.Item>
+                  );
+                })}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Link>
+        </li>
+        <li className="menu-item menu-item-has-children">
+          {/* <Link to="/sell">Sell</Link> */}
+          <Link to="#">
+            <Dropdown>
+              <Dropdown.Toggle
+                id="dropdown-basic"
+                style={{
+                  background: "white",
+                  color: "black",
+                  border: "none",
+                  fontWeight: "500",
+                  padding: "0px 2px",
+                }}
+              >
+                Sell
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                {statuslist.map((res) => {
+                  return (
+                    <Dropdown.Item
+                      href="#/action-1"
+                      style={{
+                        padding: "6% 2%",
+                        margin: "0px 8px",
+                        width: "95%",
+                      }}
+                    >
+                      <Link style={{ padding: "0" }} to={"/sell/" + res.name}>
+                        {res.name}
+                      </Link>
+                    </Dropdown.Item>
+                  );
+                })}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Link>
+        </li>
+        {/* <li className="menu-item menu-item-has-children">
+          <Link to="/buy">Buy</Link>
+        </li> */}
+        <li className="menu-item menu-item-has-children">
+          {/* <Link to="/rent">Rent</Link> */}
+          <Link to="#">
+            <Dropdown>
+              <Dropdown.Toggle
+                id="dropdown-basic"
+                style={{
+                  background: "white",
+                  color: "black",
+                  border: "none",
+                  fontWeight: "500",
+                  padding: "0px 2px",
+                }}
+              >
+                Rent
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                {statuslist.map((res) => {
+                  return (
+                    <Dropdown.Item
+                      href="#/action-1"
+                      style={{
+                        padding: "6% 2%",
+                        margin: "0px 8px",
+                        width: "95%",
+                      }}
+                    >
+                      <Link style={{ padding: "0" }} to={"/rent/" + res.name}>
+                        {res.name}
+                      </Link>
+                    </Dropdown.Item>
+                  );
+                })}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Link>
+        </li>
+        <li className="menu-item menu-item-has-children items-center">
+          <Link to="#">
+            <Dropdown>
+              <Dropdown.Toggle
+                id="dropdown-basic"
+                style={{
+                  background: "white",
+                  color: "black",
+                  border: "none",
+                  fontWeight: "500",
+                  padding: "0px 2px",
+                }}
+              >
+                Think To Know
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  href="#/action-1"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Property">
+                    How to buy property in Thailand
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-2"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Terms">
+                    Get a long terms visa in Thailand
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-3"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Estate">
+                    Thailand real estate laws
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-3"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Retire">
+                    How to retire in Thailand
+                  </Link>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Link>
+        </li>
+        <li className="menu-item menu-item-has-children items-center">
+          <Link to="#">
+            <Dropdown>
+              <Dropdown.Toggle
+                id="dropdown-basic"
+                style={{
+                  background: "white",
+                  color: "black",
+                  border: "none",
+                  fontWeight: "500",
+                  padding: "0px 2px",
+                }}
+              >
+                Finance
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  href="#/action-1"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Mortgage">
+                    How to get a mortgage loan in Thailand
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-2"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Invest">
+                    Can you invest in Thailand
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-3"
+                  style={{ padding: "6% 2%", margin: "0px 8px", width: "95%" }}
+                >
+                  <Link style={{ padding: "0" }} to="/Loans">
+                    Can you get a loans in Thailand
+                  </Link>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Link>
+        </li>
+        <li className="menu-item menu-item-has-children">
+          <Link to="/agent">Retirement Asia</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+};
+
+export default Menu;
