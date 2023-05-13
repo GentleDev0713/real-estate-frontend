c1e51cd841aa85a80e6d4266caa358a8f56b995e
GentleDev0713 <vladimirleovac7@gmail.com>
2023-05-13 14:43:42 +0100

    commit

--- src/components/layouts/Calculator.js
+++ src/components/layouts/Calculator.js
@@ -1,32 +1,99 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import FormInputGroup from "./FormInputGroup";
+// import { FcHome } from "react-icons/fc";
+import { FaDollarSign, FaPercent } from "react-icons/fa";
 
-class Calculator extends Component {
-    render() {
-        return (
-            <form>
-                <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Total Amount" name="total"  />
-                </div>
-                <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Down Payment" name="down-p"  />
-                </div>
-                <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Interest Rate" name="interest"  />
-                </div>
-                <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Loan Term" name="loan-term"  />
-                </div>
-                <div className="form-group">
-                    <select className="form-control" name="cycle">
-                        <option value="Daily">Daily</option>
-                        <option value="Monthly">Monthly</option>
-                        <option value="Yearly">Yearly</option>
-                    </select>
-                </div>
-                <button type="submit" className="btn-custom btn-block" name="button">Calculate</button>
-            </form>
-        );
-    }
-}
+const Calculator = () => {
+  const [homeValue, setHomeValue] = useState("");
+  const [downPayment, setDownPayment] = useState("");
+  const [loanAmount, setLoanAmount] = useState("");
+  const [interestRate, setInterestRate] = useState("");
+  const [loanDuration, setLoanDuration] = useState("");
+  const [monthlyPayment, setMonthlyPayment] = useState(0);
+
+  const calculateLoanAmount = () => {
+    setLoanAmount(homeValue - downPayment);
+    return loanAmount;
+  };
+
+  const calculateMonthlyPayment = () => {
+    const percentageToDecimal = (percent) => {
+      return percent / 12 / 100;
+    };
+
+    const yearsToMonths = (year) => {
+      return year * 12;
+    };
+
+    setMonthlyPayment(
+      (percentageToDecimal(interestRate) * loanAmount) /
+        (1 -
+          Math.pow(
+            1 + percentageToDecimal(interestRate),
+            -yearsToMonths(loanDuration)
+          ))
+    );
+
+    return monthlyPayment;
+  };
+
+  return (
+    <div className="mortgage" style={{ margin: "1rem auto" }}>
+      {/* <h4 className="my-5">
+        <FcHome /> Mortgage Calculator
+      </h4> */}
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <FormInputGroup
+          text="Home Value"
+          icon={<FaDollarSign />}
+          placeholder="Enter the value of the home"
+          onkeyup={calculateLoanAmount}
+          value={homeValue}
+          onInput={(e) => setHomeValue(e.target.value)}
+        />
+        <FormInputGroup
+          text="Down Payment"
+          icon={<FaDollarSign />}
+          placeholder="Enter your funds"
+          onkeyup={calculateLoanAmount}
+          value={downPayment}
+          onInput={(e) => setDownPayment(e.target.value)}
+        />
+        <FormInputGroup
+          text="Loan Amount"
+          icon={<FaDollarSign />}
+          placeholder="Funds needed"
+          readOnly={true}
+          value={loanAmount}
+        />
+        <FormInputGroup
+          text="Interest Rate"
+          icon={<FaPercent />}
+          placeholder="Enter your interest rate"
+          value={interestRate}
+          onInput={(e) => setInterestRate(e.target.value)}
+        />
+        <FormInputGroup
+          text="Loan duration (years)"
+          placeholder="Enter the duration og your loan in years"
+          value={loanDuration}
+          onInput={(e) => setLoanDuration(e.target.value)}
+        />
+        <h5 className="alert alert-info fw-bold">
+          Monthly payment: <FaDollarSign />
+          {parseFloat(monthlyPayment.toFixed(2))}
+        </h5>
+        <button
+          type="submit"
+          className="btn btn-primary btn-lg w-100 center"
+          onClick={calculateMonthlyPayment}
+        >
+          Calculate
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Calculator;
